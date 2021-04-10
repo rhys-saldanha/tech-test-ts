a8fa5cc4e31fae9fd710c7eaa824c32ee27a2419
Rhys Saldanha <rhys.saldanha@gmail.com>
2021-04-10 17:44:22 +0100

    Get popular users

--- src/userService.spec.ts
+++ src/userService.spec.ts
@@ -1,5 +1,121 @@
-import { describe, it, beforeEach } from 'mocha';
-import { expect } from 'chai';
-import { UserService } from './userService';
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+import {UserService} from './userService';
+import {DataService} from './dataService';
+import {Comment, Post, User} from './models';
 
-describe('UserService', () => {});
+
+describe('UserService', () => {
+    const commentThreshold = 2;
+    it('no data', async () => {
+        const mockDataService: DataService = getMockDataService([], [], []);
+
+        const userService = new UserService(mockDataService);
+
+        const usersWithPopularPosts = await userService.getUsersWithPopularPosts(commentThreshold);
+
+        expect(usersWithPopularPosts).to.have.length(0);
+    });
+
+    it('no posts', async () => {
+        const mockDataService: DataService = getMockDataService(
+            [{id: 1}],
+            [],
+            []
+        );
+
+        const userService = new UserService(mockDataService);
+
+        const usersWithPopularPosts = await userService.getUsersWithPopularPosts(commentThreshold);
+
+        expect(usersWithPopularPosts).to.have.length(0);
+    });
+
+    it('no comments', async () => {
+        const mockDataService: DataService = getMockDataService(
+            [{id: 1}],
+            [{id: 10, userId: 1}],
+            []
+        );
+
+        const userService = new UserService(mockDataService);
+
+        const usersWithPopularPosts = await userService.getUsersWithPopularPosts(commentThreshold);
+
+        expect(usersWithPopularPosts).to.have.length(0);
+    });
+
+    it('not enough comments', async () => {
+        const mockDataService: DataService = getMockDataService(
+            [{id: 1}],
+            [{id: 10, userId: 1}],
+            [{id: 100, postId: 10}]
+        );
+
+        const userService = new UserService(mockDataService);
+
+        const usersWithPopularPosts = await userService.getUsersWithPopularPosts(commentThreshold);
+
+        expect(usersWithPopularPosts).to.have.length(0);
+    });
+
+    it('comments on threshold', async () => {
+        const expectedUsers = [{id: 1}];
+        const mockDataService: DataService = getMockDataService(
+            expectedUsers,
+            [{id: 10, userId: 1}],
+            [{id: 100, postId: 10}, {id: 101, postId: 10}]
+        );
+
+        const userService = new UserService(mockDataService);
+
+        const usersWithPopularPosts = await userService.getUsersWithPopularPosts(commentThreshold);
+
+        expect(usersWithPopularPosts).to.deep.equal(expectedUsers);
+    });
+
+    it('comments above threshold', async () => {
+        const expectedUsers = [{id: 1}];
+        const mockDataService: DataService = getMockDataService(
+            expectedUsers,
+            [{id: 10, userId: 1}],
+            [{id: 100, postId: 10}, {id: 101, postId: 10}, {id: 102, postId: 10}]
+        );
+
+        const userService = new UserService(mockDataService);
+
+        const usersWithPopularPosts = await userService.getUsersWithPopularPosts(commentThreshold);
+
+        expect(usersWithPopularPosts).to.deep.equal(expectedUsers);
+    });
+
+    it('multiple posts, one user', async () => {
+        const expectedUsers = [{id: 1}];
+        const mockDataService: DataService = getMockDataService(
+            expectedUsers,
+            [{id: 10, userId: 1}, {id: 20, userId: 1}],
+            [{id: 100, postId: 10}, {id: 101, postId: 10},
+                {id: 102, postId: 20}, {id: 103, postId: 20}]
+        );
+
+        const userService = new UserService(mockDataService);
+
+        const usersWithPopularPosts = await userService.getUsersWithPopularPosts(commentThreshold);
+
+        expect(usersWithPopularPosts).to.deep.equal(expectedUsers);
+    });
+});
+
+function getMockDataService(users: User[], posts: Post[], comments: Comment[]): DataService {
+    return {
+        getUsers() {
+            return Promise.resolve(users);
+        },
+        getPosts() {
+            return Promise.resolve(posts);
+        },
+        getComments() {
+            return Promise.resolve(comments);
+        }
+    };
+}
--- src/userService.ts
+++ src/userService.ts
@@ -1,9 +1,34 @@
-import { DataService } from './dataService';
+import {DataService} from './dataService';
+import {Comment, Post, User} from './models';
+import {isUnique} from './utils';
 
 export class UserService {
-    constructor(private dataService: DataService) {}
+    constructor(private dataService: DataService) {
+    }
 
-    getUsersWithPopularPosts(commentThreshold: number): Promise<any[]> {
-        return Promise.resolve([]);
+    async getUsersWithPopularPosts(commentThreshold: number): Promise<User[]> {
+        let users = await this.dataService.getUsers();
+        let posts = await this.dataService.getPosts();
+        let comments = await this.dataService.getComments();
+
+        let popularPostUserIds = posts
+            .filter(post => this.isPopular(post, commentThreshold, comments))
+            .map(post => post.userId);
+
+        let popularUsers = popularPostUserIds
+            .filter(isUnique(popularPostUserIds))
+            .map(userId => this.getUser(userId, users));
+
+        return Promise.resolve(popularUsers);
+    }
+
+    private isPopular(post: Post, commentThreshold: number, comments: Comment[]): boolean {
+        return comments
+            .filter(comment => comment.postId === post.id)
+            .length >= commentThreshold;
+    }
+
+    private getUser(userId: number, users: User[]): User {
+        return users.find(user => user.id === userId)!;
     }
 }
